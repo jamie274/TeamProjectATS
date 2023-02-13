827719ad0a90dad68a8e3d2c3b7fb7970056d4ad
jamie274 <jamie-lee.gordon@city.ac.uk>
2023-02-13 20:44:03 +0000

    admin login and sales advisor login pages designs added

--- src/LoginPages/AdminLogin.java
+++ src/LoginPages/AdminLogin.java
@@ -1,13 +1,15 @@
+package LoginPages;
+
 /**
  *
  * @author gordo
  */
-public class Login extends javax.swing.JFrame {
+public class AdminLogin extends javax.swing.JFrame {
 
     /**
-     * Creates new form Login
+     * Creates new form LoginPages.Login
      */
-    public Login() {
+    public AdminLogin() {
         initComponents();
     }
 
@@ -53,7 +55,7 @@ public class Login extends javax.swing.JFrame {
         });
 
         jLabel4.setFont(new java.awt.Font("Rockwell", 0, 24)); // NOI18N
-        jLabel4.setText("Admin Login");
+        jLabel4.setText("Admin LoginPages.Login");
 
         jLabel1.setText("Username");
 
@@ -156,20 +158,20 @@ public class Login extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(AdminLogin.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Login().setVisible(true);
+                new AdminLogin().setVisible(true);
             }
         });
     }
