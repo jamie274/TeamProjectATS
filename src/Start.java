430f04660815811b3322d1f7ca4b5ccca8f088ce
jamie274 <jamie-lee.gordon@city.ac.uk>
2023-03-15 18:10:47 +0000

    Staff id and name is now shown on their menus

--- src/Start.java
+++ src/Start.java
@@ -5,19 +5,6 @@ import java.sql.SQLException;
 public class Start {
     public static void main(String args[]) throws SQLException {
         new WelcomePage().setVisible(true);
-        //test();
-    }
-
-    public static void test () throws SQLException {
-        int id = 200081454;
-        String pwd = "SivaT##_01";
-        SQLLoginHelper s = new SQLLoginHelper();
-
-        if (s.attemptLogin("Office Manager", id, "SivaT##_01")) {
-            System.out.println("yes");
-        } else {
-            System.out.println("no");
-        }
     }
 }
 
