21450f7fcc27d151cb556c727cacb6fe4d6dfd35
jamie274 <jamie-lee.gordon@city.ac.uk>
2023-03-18 15:59:13 +0000

    Refresh blanks button added

--- src/Start.java
+++ src/Start.java
@@ -2,8 +2,7 @@ import LoginPages.*;
 
 public class Start {
     public static void main(String args[]) {
-        //new WelcomePage().setVisible(true);
-        new AdministrationMenu("200006572", "Abdul").setVisible(true);
+        new WelcomePage().setVisible(true);
     }
 }
 
